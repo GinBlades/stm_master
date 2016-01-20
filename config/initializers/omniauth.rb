14adb8f0879f58a5380d3240aba1e48c206bf23d
Shaun Martin <webmaster@ginblades.com>
2016-01-20 09:21:21 -0600

    Using application secrets for omniauth config.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,12 @@
-APP_ID = ENV['AUTH_PROVIDER_APPLICATION_ID'] || "not_a_real_id"
-APP_SECRET = ENV['AUTH_PROVIDER_SECRET'] || "not_a_real_secret"
+PROVIDER = Rails.application.secrets.auth_provider_url
+SETUP_PROC = lambda do |env|
+  env['omniauth.strategy'].options[:client_id] = Rails.application.secrets.auth_provider_application_id
+  env['omniauth.strategy'].options[:client_secret] = Rails.application.secrets.auth_provider_secret
+  env['omniauth.strategy'].options[:client_options].site = PROVIDER
+  env['omniauth.strategy'].options[:client_options].authorize_url = "#{PROVIDER}/oauth/authorize"
+  env['omniauth.strategy'].options[:client_options].access_token_url = "#{PROVIDER}/oauth/token"
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :so, APP_ID, APP_SECRET
+  provider :so, setup: SETUP_PROC
 end
